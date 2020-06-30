a6cc7a74e133a356a4fb38167d4bad96749ae5bb
wchya <wchhuangyawork@163.com>
2020-06-30 08:55:59 +0800

    1. 新增 css 练习的控制器，新增智能表单css练习页面（未完成）；

---
+++ wjhxts-web/src/main/java/com/ch/wchya/wjhxts/web/controller/CssController.java
@@ -0,0 +1,21 @@
+package com.ch.wchya.wjhxts.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @program: wjhxts
+ * @description: css样式练习控制器
+ * @author: 王超
+ * @create: 2020-06-29 22:48
+ **/
+@Controller
+@RequestMapping("/css")
+public class CssController {
+
+    @GetMapping("/form/smart")
+    public String smart() {
+        return "css/form_smart";
+    }
+}
