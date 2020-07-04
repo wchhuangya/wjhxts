0fc3ba5fe2af4dace1ff2f04afc8a1b6814fc712
wchya <wchhuangyawork@163.com>
2020-07-04 10:43:27 +0800

    1. 新增 html hn-p 个人简介练习；

---
+++ wjhxts-web/src/main/java/com/ch/wchya/wjhxts/web/controller/HtmlController.java
@@ -0,0 +1,21 @@
+package com.ch.wchya.wjhxts.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @program: wjhxts
+ * @description: Html 练习控制器
+ * @author: 王超
+ * @create: 2020-07-03 20:56
+ **/
+@Controller
+@RequestMapping("/html")
+public class HtmlController {
+
+    @GetMapping("/hn-p/intro")
+    public String intro() {
+        return "html/hn-p_intro";
+    }
+}
